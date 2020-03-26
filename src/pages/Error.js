e990fdcb4c2cc3d6e89234a7b285cba0dc39bcd3
piyush.nanwani <pnanwani61@gmail.com>
2020-03-26 05:33:00 +0530

    updated Our rooms and home page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Hero from "../components/Hero"
-const Error = () => {
-    return <Hero/>;
+import Banner from "../components/Banner"
+import {Link} from 'react-router-dom'
+export default function Error() {
+    return <Hero> 
+        <Banner title='404' subtitle="page not found">
+            <Link to='/' className='btn-primary'>
+                return home
+            </Link>
+        </Banner>
+        </Hero>;
 }
-
-export default Error
